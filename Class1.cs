a018aca05cbd8cee66b4f419ecd444c8a3fa2927
Sathvik2401 <136308979+Sathvik2401@users.noreply.github.com>
2023-06-12 14:49:42 +0530

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assembly1
+{
+    public class Assembly1Class1
+    {
+        protected internal int Id = 101;
+
+    }
+    public class Assembly1Class2
+    {
+        public void SampleMethod()
+        {
+            Assembly1Class1 A1 = new Assembly1Class1();
+            Console.WriteLine(A1.Id);
+            Console.ReadKey();
+        }
+    }
+
+}
